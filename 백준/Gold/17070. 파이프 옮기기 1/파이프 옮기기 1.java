69f06a481ea853701c5e1fe7253b13302d057e20
wonu606 <133995055+wonu606@users.noreply.github.com>
2023-07-17 13:15:04 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 132 ms, Memory: 14516 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,10 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 public class Main {
 
@@ -24,128 +21,75 @@ public class Main {
                     .toArray(Integer[]::new);
         }
 
-        List<Pipe> pipes = new ArrayList<>();
+        // 0: 가로, 1: 대각, 2: 세로
+        int[][][] dp = new int[size][size][3];
+        dp[0][0][0] = 1;
+        for (int col = 1; col < size; col++) {
+            if (map[0][col] == 1) {
+                continue;
+            }
+            dp[0][col][0] += dp[0][col - 1][0];
+            dp[0][col][1] += dp[0][col - 1][0];
+        }
 
-        // 0
-        Pipe rightDirectionPipe = new Pipe();
-        rightDirectionPipe.nextCoordinate = new Coordinate(0, 1);
-        rightDirectionPipe.addRequiredCoordinates(
-                new Coordinate(0, 1)
-        );
-        pipes.add(rightDirectionPipe);
+        for (int row = 1; row < size; row++) {
+            for (int col = 1; col < size; col++) {
+                if (map[row][col] == 1) {
+                    continue;
+                }
 
-        // 1
-        Pipe rightDownDirectionPipe = new Pipe();
-        rightDownDirectionPipe.nextCoordinate = new Coordinate(1, 1);
-        rightDownDirectionPipe.addRequiredCoordinates(
-                new Coordinate(0, 1), new Coordinate(1, 1),
-                new Coordinate(1, 0)
-        );
-        pipes.add(rightDownDirectionPipe);
+                /*
+                다음 가로: 이전이 가로이거나, 대각일 경우에만 가능
+                다음 대각: 이전이 가로, 대각, 세로
+                다음 세로: 이전 대각, 세로
+                 */
 
-        // 2
-        Pipe downDirectionPipe = new Pipe();
-        downDirectionPipe.nextCoordinate = new Coordinate(1, 0);
-        downDirectionPipe.addRequiredCoordinates(
-                new Coordinate(1, 0)
-        );
-        pipes.add(downDirectionPipe);
+                dp[row][col][0] += dp[row][col - 1][0];
+                dp[row][col][1] += dp[row][col - 1][0];
 
-        Coordinate startCoordinate = new Coordinate(0, 1);
-        Coordinate endCoordinate = new Coordinate(size - 1, size - 1);
-        int result = dfs(map, pipes, 0, startCoordinate, endCoordinate, 0);
+                if (map[row][col - 1] == 0 && map[row - 1][col] == 0) {
+                    dp[row][col][0] += dp[row - 1][col - 1][1];
+                    dp[row][col][1] += dp[row - 1][col - 1][1];
+                    dp[row][col][2] += dp[row - 1][col - 1][1];
+                }
+
+                dp[row][col][1] += dp[row - 1][col][2];
+                dp[row][col][2] += dp[row - 1][col][2];
+            }
+        }
+
+//        for (int row = 0; row < size; row++) {
+//            System.out.print('[');
+//            for (int col = 0; col < size; col++) {
+//                System.out.print(map[row][col] + ", ");
+//            }
+//            System.out.print("]\n");
+//        }
+//
+//        for (int row = 0; row < size; row++) {
+//            for (int col = 0; col < size; col++) {
+//                System.out.print("[");
+//                Arrays.stream(dp[row][col])
+//                        .forEach(e -> System.out.print(e + ","));
+//                System.out.print("]\t");
+//            }
+//            System.out.println();
+//        }
+
+        int result = 0;
+        result += dp[size - 1][size - 2][0];
+        if (map[size - 1][size - 2] == 0 && map[size - 2][size - 1] == 0) {
+            result += dp[size - 2][size - 2][1];
+        }
+        result += dp[size - 2][size - 1][2];
+
+        if (map[size - 1][size - 1] == 1) {
+            result = 0;
+        }
 
         writer.write(String.valueOf(result));
 
         reader.close();
         writer.close();
-    }
-
-    private static Integer dfs(Integer[][] map, List<Pipe> pipes, int prevPipeIndex, Coordinate currentCoordinate, Coordinate endCoordinate, int result) {
-        if (Objects.equals(currentCoordinate, endCoordinate)) {
-            result++;
-            return result;
-        }
-
-        Integer nextResult = result;
-        for (int currentPipeIndex = 0; currentPipeIndex < pipes.size(); currentPipeIndex++) {
-            if (prevPipeIndex == 0 && currentPipeIndex == 2) {
-                continue;
-            }
-            if (prevPipeIndex == 2 && currentPipeIndex == 0) {
-                continue;
-            }
-            Pipe currentPipe = pipes.get(currentPipeIndex);
-
-            if (!validate(map, currentCoordinate, currentPipe)) {
-                continue;
-            }
-            Coordinate nextCoordinate =
-                    new Coordinate(currentCoordinate.row + currentPipe.nextCoordinate.row,
-                            currentCoordinate.col + currentPipe.nextCoordinate.col);
-            nextResult = dfs(map, pipes, currentPipeIndex, nextCoordinate, endCoordinate, nextResult);
-        }
-        return nextResult;
-    }
-
-    private static boolean validate(Integer[][] map, Coordinate currentCoordinate, Pipe selectedPipe) {
-        for (Coordinate requiredCoordinate : selectedPipe.requiredCoordinates) {
-            Coordinate coordinate = new Coordinate(currentCoordinate.row + requiredCoordinate.row,
-                    currentCoordinate.col + requiredCoordinate.col);
-            if (!validateRange(map, coordinate)) {
-                return false;
-            }
-            if (map[coordinate.row][coordinate.col] == 1) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static boolean validateRange(Integer[][] map, Coordinate coordinate) {
-        if (coordinate.row < 0 || map.length <= coordinate.row) {
-            return false;
-        }
-        if (coordinate.col < 0 || map[coordinate.row].length <= coordinate.col) {
-            return false;
-        }
-        return true;
-    }
-
-    static class Pipe {
-
-        Coordinate nextCoordinate;
-        List<Coordinate> requiredCoordinates = new ArrayList<>();
-
-        void addRequiredCoordinates(Coordinate... coordinates) {
-            requiredCoordinates.addAll(Arrays.asList(coordinates));
-        }
-    }
-
-    static class Coordinate {
-        int row;
-        int col;
-
-        public Coordinate(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Coordinate that = (Coordinate) o;
-            return row == that.row && col == that.col;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(row, col);
-        }
     }
 }
