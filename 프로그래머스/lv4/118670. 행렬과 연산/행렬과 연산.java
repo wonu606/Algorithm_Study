7fea0c41801ff54ef93b89f280ddaab2dc77e8a7
wonu606 <133995055+wonu606@users.noreply.github.com>
2023-09-28 10:18:26 +0900

    [level 4] Title: 행렬과 연산, Time: 56.24 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/lv4/118670. 행렬과 연산/행렬과 연산.java
+++ 프로그래머스/lv4/118670. 행렬과 연산/행렬과 연산.java
@@ -1,64 +1,71 @@
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 
+/**
+ * `ShiftRow`와 `Rotate`를 구현하고 각 방법을 적용해볼까? 행과 열의 길이 50_000 * 50_000 operations의 길이 100_000 최대 50_000 * 50_000 * 100_000
+ * 연산이 일어날 수 있어 무리가 있을 것 같다. (10억번의 연산 안에 풀어야 함)
+ * <p>
+ * operations은 줄일 수 없고 1번씩 수행해야 할테고, 25억개의 원소가 들어있는 맵을 전부 들여보는 형태가 아닌 다른 방법을 찾아봐야겠다.
+ */
 class Solution {
 
     public int[][] solution(int[][] rc, String[] operations) {
 
         // 데크 생성
-        Deque<Integer> firstDeque = new ArrayDeque<>();
-        Deque<Deque<Integer>> midDeque = new ArrayDeque<>();
-        Deque<Integer> lastDeque = new ArrayDeque<>();
-
-        for (int[] row : rc) {
-            firstDeque.addLast(row[0]);
-
-            midDeque.addLast(new ArrayDeque<>());
-            for (int col = 1; col < row.length - 1; col++) {
-                midDeque.getLast().addLast(row[col]);
+        Deque<Integer>[] dequeArray = new Deque[rc.length];
+        for (int row = 0; row < rc.length; row++) {
+            dequeArray[row] = new ArrayDeque<>();
+            for (int col = 0; col < rc[row].length; col++) {
+                dequeArray[row].addLast(rc[row][col]);
             }
-
-            lastDeque.addLast(row[row.length - 1]);
         }
 
-        // 연산
+        // 연산 수행
         for (String operation : operations) {
             switch (operation) {
-                case "ShiftRow" -> shiftRow(firstDeque, midDeque, lastDeque);
-                case "Rotate" -> rotate(firstDeque, midDeque, lastDeque);
-                default -> throw new IllegalArgumentException("수행할 수 없는 연산입니다.");
+                case "ShiftRow":
+                    shiftRow(dequeArray);
+                    break;
+                case "Rotate":
+                    rotate(dequeArray);
+                    break;
+                default:
+                    throw new IllegalArgumentException("정의되지 않은 연산입니다.");
             }
         }
 
-        // Deque -> int array
-        int[][] result = new int[rc.length][];
-        for (int row = 0; row < result.length; row++) {
-            result[row] = new int[rc[row].length];
-
-            result[row][0] = firstDeque.removeFirst();
-
-            Deque<Integer> mid = midDeque.removeFirst();
-            for (int col = 1; col < rc[row].length - 1; col++) {
-                result[row][col] = mid.removeFirst();
+        // 결과 리턴
+        int[][] resultRc = new int[dequeArray.length][];
+        for (int row = 0; row < dequeArray.length; row++) {
+            resultRc[row] = new int[dequeArray[row].size()];
+            for (int col = 0; col < resultRc[row].length; col++) {
+                resultRc[row][col] = dequeArray[row].removeFirst();
             }
-
-            result[row][rc[row].length - 1] = lastDeque.removeFirst();
         }
-        return result;
+        return resultRc;
     }
 
-    private void shiftRow(Deque<Integer> firstDeque, Deque<Deque<Integer>> midDeque, Deque<Integer> lastDeque) {
-        firstDeque.addFirst(firstDeque.removeLast());
-        midDeque.addFirst(midDeque.removeLast());
-        lastDeque.addFirst(lastDeque.removeLast());
+    // 최대 100_000 연산
+    private void shiftRow(Deque<Integer>[] dequeArray) {
+        Deque<Integer> lastDeque = new ArrayDeque<>(dequeArray[dequeArray.length - 1]);
+
+        for (int row = dequeArray.length - 1; row > 0; row--) {
+            dequeArray[row] = dequeArray[row - 1];
+        }
+        dequeArray[0] = lastDeque;
     }
 
-    private void rotate(Deque<Integer> firstDeque, Deque<Deque<Integer>> midDeque, Deque<Integer> lastDeque) {
-        midDeque.getFirst().addFirst(firstDeque.removeFirst());
-        lastDeque.addFirst(midDeque.getFirst().removeLast());
-        midDeque.getLast().addLast(lastDeque.removeLast());
-        firstDeque.addLast(midDeque.getLast().removeFirst());
+    // 최대 100_000 연산
+    private void rotate(Deque<Integer>[] dequeArray) {
+        for (int row = dequeArray.length - 1; row > 0; row--) {
+            Integer last = dequeArray[row - 1].removeLast();
+            dequeArray[row].addLast(last);
+        }
+
+        for (int row = 0; row + 1 < dequeArray.length; row++) {
+            Integer first = dequeArray[row + 1].removeFirst();
+            dequeArray[row].addFirst(first);
+        }
     }
 
     public static void main(String[] args) {
@@ -72,10 +79,6 @@ class Solution {
         };
 
         Solution solution = new Solution();
-        int[][] result = solution.solution(rc, operations);
-
-        for (int[] row : result) {
-            System.out.println(Arrays.toString(row));
-        }
+        solution.solution(rc, operations);
     }
 }
